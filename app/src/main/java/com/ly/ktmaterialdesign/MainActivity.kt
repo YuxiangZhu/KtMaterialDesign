9ca7b8c666693c4e0f6d29f275174dc7ca930773
alex.zhu <alex.zhu@ly.com>
2020-05-15 18:15:46 +0800

    Initial commit

---
+++ app/src/main/java/com/ly/ktmaterialdesign/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ly.ktmaterialdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * MaterialDesign
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
