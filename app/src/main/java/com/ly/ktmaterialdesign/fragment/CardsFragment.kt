f0fb21464a35ba03347be9bfd1fdc35029c12fa2
alex.zhu <alex.zhu@ly.com>
2020-06-03 17:09:34 +0800

    card done

--- app/src/main/java/com/ly/ktmaterialdesign/fragment/CardsFragment.kt
+++ app/src/main/java/com/ly/ktmaterialdesign/fragment/CardsFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.ly.ktmaterialdesign.R
 import com.ly.ktmaterialdesign.databinding.FragmentCardsBinding
+import kotlinx.android.synthetic.main.card_1.*
+import kotlinx.android.synthetic.main.card_2.*
 
 /**
  * 作者： Alex
@@ -24,6 +29,11 @@ class CardsFragment : Fragment() {
     ): View? {
 
        val binding = FragmentCardsBinding.inflate(inflater)
+
+        Glide.with(activity!!).load(R.drawable.material_design_5)
+            .apply(RequestOptions().fitCenter()).into(binding.card1.imgCard1)
+        Glide.with(context!!).load(R.drawable.material_design_4)
+            .apply(RequestOptions().fitCenter()).into(binding.card2.imgCard2)
 
 
         return binding.root
