e5f6a9092f9cc9a88a04c638a204117593d28001
alex.zhu <alex.zhu@ly.com>
2020-06-04 17:18:29 +0800

    widget done

--- app/src/main/java/com/ly/ktmaterialdesign/fragment/WidgetsFragment.kt
+++ app/src/main/java/com/ly/ktmaterialdesign/fragment/WidgetsFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.blankj.utilcode.util.LogUtils
+import com.ly.ktmaterialdesign.R
 import com.ly.ktmaterialdesign.databinding.FragmentWidgetsBinding
+import kotlinx.android.synthetic.main.fragment_widgets.view.*
 
 /**
  * 作者： Alex
@@ -24,6 +27,34 @@ class WidgetsFragment: Fragment() {
     ): View? {
 
         val binding = FragmentWidgetsBinding.inflate(inflater)
+
+        binding.toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+
+            when(checkedId){
+                R.id.btn1 ->{
+
+                    LogUtils.e("btn1$isChecked")
+
+                }
+            }
+
+        }
+
+
+        binding.chipgroup.setOnCheckedChangeListener { group, checkedId ->
+
+            when(checkedId){
+                R.id.chip1 ->{
+                    LogUtils.e("chip1")
+                }
+
+                R.id.chip2 ->{
+                    LogUtils.e("chip2")
+                }
+
+            }
+
+        }
 
         return binding.root
     }
